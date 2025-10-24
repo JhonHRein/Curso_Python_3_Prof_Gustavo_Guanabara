344b4b2239f585550178f75fb9cce074967838cd
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-10-24 18:22:30 -0300

    Create Aula 01 - Primeiro Contato.py
    Introdução ao Python, primeiros passos, primeiros comandos, modulo 1/3.

---
+++ Módulo1_Básico/Aula 01 - Primeiro Contato.py
@@ -0,0 +1,9 @@
+print("Olá Mundo!")
+nome = input("Qual é seu nome ? ")
+print(f"É um prazer te conhecer, {nome}")
+
+peso = input("Qual o seu peso? ")
+altura = input("Qual a sua altura? ")
+idade = input("Qual a sua idade? ")
+print()
+print(f"Nome: {nome}\nAltura: {altura}m\nIdade: {idade} anos.")
