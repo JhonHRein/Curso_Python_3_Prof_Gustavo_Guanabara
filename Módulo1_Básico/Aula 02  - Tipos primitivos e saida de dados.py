344757abbb7e8f8f2601b525e3b40628d5a6777a
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-10-27 09:41:22 -0300

    Create Aula 02  - Tipos primitivos e saida de dados.py
    Aula teórica, exemplos de tipos primitivos.

---
+++ Módulo1_Básico/Aula 02  - Tipos primitivos e saida de dados.py
@@ -0,0 +1,19 @@
+# Tipos primitivos e saida de dados.
+
+'''Input = entrada de dados
+float = numeros com flutuantes, ou com pontuaçao de divisao de casas decimais ex: 1.3
+int =  numeros inteiros ex: 1, 2, 3
+str =  strings, para sequencias de letras ou textos.
+bool =  booleano, validaçao verdadeiro ou falso,  True/False'''
+
+print()
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro valor: "))
+s = n1 + n2 
+print(f"A soma entre {n1} e {n2} é um total de {s} ")
+
+print()
+n3 = float(input("Digite um numero: "))
+n4 = float(input("Digite outro valor: "))
+s = n3 + n4 
+print(f"A soma entre {n3} e {n4} é um total de {s} ")
