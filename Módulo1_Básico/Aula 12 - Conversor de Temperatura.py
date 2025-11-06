8d5a608ad8d9294f35541f6b838f831ad8a71936
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-11-06 18:15:25 -0300

    Create Aula 12 - Conversor de Temperatura.py

---
+++ Módulo1_Básico/Aula 12 - Conversor de Temperatura.py
@@ -0,0 +1,6 @@
+#Escreva um programa que converta uma temperatura digitando em graus celsius e Convertendo para Fahrenheit.
+print()
+print("_"*20)
+celsius = float(input("Conversor de temperatura: ºC para ºf, digite o valor: "))
+fahrenheit = ((9*celsius)/5)+32
+print(f"A temperatura de {celsius}ºC corresponde a {fahrenheit}ºF:")
