dd8058e5d4597a84d0e731a3e673745b22114735
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-12-01 16:40:39 -0300

    Delete Aula 08 - Conversor de Medidas.py

--- Módulo1_Básico/Aula 08 - Conversor de Medidas.py
+++
@@ -1,8 +0,0 @@
-# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
-print()
-
-metros = float(input("Digite o valor da sua metragem: "))
-centimetros = metros * 100
-milimetros = metros * 1000
-print()
-print(f"- O valor de: {metros}m, convertido em centimetros: {centimetros}\n- Convertido em milimetros: {milimetros}")
