54253da198dd00fe04057ee787ca14dc18bcab98
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-12-01 16:40:25 -0300

    Delete Aula 05.1 - Antecessor e sucessor.py

--- Módulo1_Básico/Aula 05.1 - Antecessor e sucessor.py
+++
@@ -1,9 +0,0 @@
-#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.")
-print()
-
-numero1 = int(input("Digite um valor :"))
-sucessor = numero1 - 1
-antecessor = numero1 + 1
-
-print(f"O sucessor de {numero1} é: {sucessor}")
-print(f"O antecessor de {numero1} é: {antecessor}")
