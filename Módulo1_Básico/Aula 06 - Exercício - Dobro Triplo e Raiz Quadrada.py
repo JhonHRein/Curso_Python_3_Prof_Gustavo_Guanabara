f13e3b8d70101c5545a29a84cc587c60e2105026
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-12-01 16:38:39 -0300

    Create Aula 06 - Exercício - Dobro Triplo e Raiz Quadrada.py

---
+++ Módulo1_Básico/Aula 06 - Exercício - Dobro Triplo e Raiz Quadrada.py
@@ -0,0 +1,10 @@
+#Exercício: Crie um algoritimo que leia um numero e mostre o seu dobro, triplo e a raiz quadrada.
+print()
+
+numero2 = int(input("Digite um valor: "))
+dobro = numero2 * 2
+triplo = numero2 * 3
+raiz_q = numero2 ** 0.5
+print(f"- O dobro do valor: {numero2} é {dobro}")
+print(f"- O triplo: {triplo}")
+print(f"- Raiz quadrada é {raiz_q:.2f}")
