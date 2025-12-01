265c0752966d5fa38b4a3bf47694500d0ac7afd2
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-12-01 16:40:45 -0300

    Delete Aula 11 - Reajuste Salarial.py

--- Módulo1_Básico/Aula 11 - Reajuste Salarial.py
+++
@@ -1,5 +0,0 @@
-#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-print()
-salario = float(input("Digite o valor do seu ordenado salarial: "))
-salario_aumento_15 = salario + ((salario * 15) / 100)
-print(f"O valor do salario atualizado com 15% é de: R$ {salario_aumento_15:.2f} reais;")
