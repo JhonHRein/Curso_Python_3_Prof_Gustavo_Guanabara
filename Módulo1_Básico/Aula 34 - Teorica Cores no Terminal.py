aba3bb34d2f9a4e02e00677aeaf8212509679253
JHON HERBERT REIN <jhon.rein@hotmail.com>
2026-01-09 17:38:17 -0300

    Create Aula 34 - Teorica Cores no Terminal.py

---
+++ Módulo1_Básico/Aula 34 - Teorica Cores no Terminal.py
@@ -0,0 +1,11 @@
+print()
+print()
+print("TESTE DE COR")
+# Codigo ANSI começa com contra barra '\' codigo '033' abre colchetes '[' letra 'm'no final dentro das aspas.
+# \033[m   <<<<<<<< CODIGO PADRAO
+#print("\033[4;33;44mOla Mundo\033[m") O CODIGO \033[m no final da str serve para limitar a faixa fundo de cor.
+print()
+print()
+print("*"*50)
+print("\033[4;33;44mOla Mundo\033[m")
+print("\033[0;30;41mTodo São Paulino é Gay\033[m")
